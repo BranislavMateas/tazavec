4c074ecb14a0f8cc7d4eab44d2eea5dba98da0f0
Branislav Mateáš <mateas.branislav@gmail.com>
2024-07-10 18:57:21 +0200

    Updated default skeleton

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tazavec/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Tazavec",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
