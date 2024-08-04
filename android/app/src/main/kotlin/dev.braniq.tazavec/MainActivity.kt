d58c8c590caeac81796e11522ba8e54ae023fadd
Branislav Mateáš <mateas.branislav@gmail.com>
2024-08-04 12:30:28 +0200

    Added google_mobile_ads_sdk

--- android/app/src/main/kotlin/dev.braniq.tazavec/MainActivity.kt
+++ android/app/src/main/kotlin/dev.braniq.tazavec/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tazavec
+package dev.braniq.tazavec
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
-import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:loggy/loggy.dart';
 import 'package:tazavec/app.dart';
 
 const String appTitle = "Tazavec";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  unawaited(MobileAds.instance.initialize());
+
   Loggy.initLoggy();
 
   runApp(const App());
